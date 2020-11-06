0447833c43e9631405a1d513b90df5db95e2acbb
Jorge Barrera <Jorge2013701@gmail.com>
2020-11-05 21:19:01 -0400

    iconizacion

--- src/helpers/laboratorio.js
+++ src/helpers/laboratorio.js
@@ -52,3 +52,32 @@ export const evaluacionDireccionBaja = (X,A,B,C,D)=>{
     }
 }
 
+
+export const iconizacionDM = (examen1,examen2,examen3,examen4)=>{
+
+    if( examen1 === "peligroso" || examen2 === "peligroso" || examen3 === "peligroso" || examen4 === "peligroso"){
+       
+        const resultado = "fa-exclamation-triangle text-warning";
+        
+        return resultado;
+    }else if (examen1 === "bajito" || examen2 === "bajito" || examen3 === "bajito" || examen4 === "bajito"){
+        
+        const resultado = "fa-arrow-down text-danger";
+        
+        return resultado;
+    }else if(examen1 === "malo" || examen2 === "malo" || examen3 === "malo" || examen4 === "malo"){
+
+        const resultado = "fa-times text-danger";
+      
+        return resultado;
+
+    }else if(examen1 === "mediocre" || examen2 === "mediocre" || examen3 === "mediocre" || examen4 === "mediocre"){
+        const resultado = "fa-plus fa-minus text-warning "
+    }else{
+        const resultado = "fa-check text-success";
+       
+        return resultado;
+    }
+
+}
+
